c9bb6093df07776eb0c69d9e119064322a616170
Anne-Claire Ho Si <anne-claire.ho-si@alan.eu>
2025-03-03 13:53:59 +0100

    (feat) Frontend: Operations table

--- src/features/app-home/PageHome.tsx
+++ src/features/app-home/PageHome.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import { Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import { Flex, Heading, Spacer, Stack, Table, Text } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 import { Logo } from '@/components/Logo';
 import { AppLayoutPage } from '@/features/app/AppLayoutPage';
+import { trpc } from '@/lib/trpc/client';
+import { LoaderFull } from '@/components/LoaderFull';
+import { ErrorPage } from '@/components/ErrorPage';
+import { Button } from '@/components/ui/button';
 
 export default function PageHome() {
   const { t } = useTranslation(['appHome', 'account']);
+
+  const { data: projects, isLoading, isError } = trpc.projects.getProjectsForCurrentUser.useQuery()
+  const currentProject = projects? projects[0] : null;  // TODO: Implement Select project
+
+  if (isLoading) {
+    return <LoaderFull />;
+  }
+
+  if (isError) {
+    return <ErrorPage />;
+  }
 
   return (
     <AppLayoutPage>
@@ -22,11 +37,56 @@ export default function PageHome() {
         </Flex>
 
         <Stack>
-          <Heading fontSize="lg">{t('appHome:welcome.title')}</Heading>
-          <Text display="block">
-            {t('appHome:welcome.description')}
-            <br />
-          </Text>
+          <Heading fontSize="4xl">{t('appHome:projectTab:title')}</Heading>
+          <Spacer/>
+          <Table.Root>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>{t('appHome:projectTab:operationsTable.nameHeader')}</Table.ColumnHeader>
+                <Table.ColumnHeader>{t('appHome:projectTab:operationsTable.ademeCodeHeader')}</Table.ColumnHeader>
+                <Table.ColumnHeader>{t('appHome:projectTab:operationsTable.statusHeader')}</Table.ColumnHeader>
+                <Table.ColumnHeader>{t('appHome:projectTab:operationsTable.attestationRgeHeader')}</Table.ColumnHeader>
+                <Table.ColumnHeader></Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {
+                currentProject?.operations?.map((operation) => {
+                  return (
+                    <Table.Row>
+
+                    {/* OPERATION NAME */}
+                    <Table.Cell>
+                      <Text display="block">{operation.name}</Text>
+                    </Table.Cell>
+
+                      {/* CODE ADEME */}
+                      <Table.Cell>
+                        <Text display="block">{operation.ademeCode}</Text>
+                      </Table.Cell>
+
+                      {/* STATUT */}
+                      <Table.Cell>
+                        {/* TODO: {operation.documents.any((document) => document.docType == 'ATTESTATION_RGE')} */}
+                      </Table.Cell>
+
+                      {/* ATTESTATION */}
+                      <Table.Cell>
+                      {/* TODO: Dropdown list */}
+                      </Table.Cell>
+
+                      {/* PREVIEW */}
+                      <Table.Cell>
+                        <Button onClick={()=> {/* TODO: Preview PDF */}}>
+                          Voir
+                        </Button>
+                      </Table.Cell>
+                    </Table.Row>)
+                })}
+            </Table.Body>
+          </Table.Root>
+          <Spacer />
         </Stack>
       </Stack>
     </AppLayoutPage>
